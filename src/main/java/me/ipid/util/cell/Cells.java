3d0433ecc9340fb57c070789731009e9e70cb718
ipid <ipid@users.noreply.github.com>
2020-03-24 20:31:38 +0800

    Add emptyX methods

--- src/main/java/me/ipid/util/cell/Cells.java
+++ src/main/java/me/ipid/util/cell/Cells.java
@@ -12,6 +12,38 @@ public class Cells {
         return new Cell<>(null);
     }
 
+    public static CellByte emptyByte() {
+        return new CellByte((byte) 0);
+    }
+
+    public static CellShort emptyShort() {
+        return new CellShort((short) 0);
+    }
+
+    public static CellInt emptyInt() {
+        return new CellInt(0);
+    }
+
+    public static CellLong emptyLong() {
+        return new CellLong(0L);
+    }
+
+    public static CellFloat emptyFloat() {
+        return new CellFloat(0f);
+    }
+
+    public static CellDouble emptyDouble() {
+        return new CellDouble(0d);
+    }
+
+    public static CellChar emptyChar() {
+        return new CellChar('\u0000');
+    }
+
+    public static CellBool emptyBool() {
+        return new CellBool(false);
+    }
+
     public static CellByte of(byte elem) {
         return new CellByte(elem);
     }
